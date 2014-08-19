ec36dc70801e71da7184112278a78d12c1bcb793
gcid <gcid12@gmail.com>
2014-08-19 19:43:25 -0400

    Configuring Data

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -5,10 +5,23 @@ class DemoController < ApplicationController
   def index
   	#render(:template => 'demo/hello')
   	# render('demo/hello')
-  	render('hello')  #you dont need to add .html
+  	render('index')  #you dont need to add .html
   end
 
   def hello
-  	render('index') 
+  	#render('hello') 
+    @array = [1,2,3,4,5]   #this is an instance variable
+    @id = params['id'].to_i
+    @page = params[:page].to_i
   end	
+
+  def other_hello
+  		redirect_to(:controller => 'demo', :action => 'index')
+  end	
+
+  def lynda
+    redirect_to("http:tcb.io")
+    
+  end
+
 end
